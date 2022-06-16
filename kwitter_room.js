8472bdccda7a97c9a06d50ce606f4acc6ee3d33b
Bcez <80232341+Bcez@users.noreply.github.com>
2022-06-15 18:34:59 -0700

    x

--- kwitter_room.js
+++ kwitter_room.js
@@ -36,10 +36,16 @@ var firebaseConfig = {
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
-
-      
+       row = "<div class='room_name' id ="+Room_names+" onclick='redirectToRoomName(this.id)' > #" + Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
       });});}
 getData();
+
+function redirectToRoomName(name) {
+
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
 
 function logout() {
       localStorage.removeItem("user_name");
